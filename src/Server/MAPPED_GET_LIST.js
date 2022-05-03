66f636e316f3a0a9edb2238305b535ef8b3f0144
Nurmyrat-AN <nurmyratnurmyrat457@gmail.com>
2022-05-04 01:52:29 +0500

    1

--- src/Server/MAPPED_GET_LIST.js
+++ src/Server/MAPPED_GET_LIST.js
@@ -18,7 +18,7 @@ class MAPPED_GET_LIST {
                                         ${this.params.search ? `name LIKE '%${this.replaceSpaceToQuery(this.params.search)}' ` : '1=1'} ORDER BY tertip, name`))
         }),
         brands: async () => ({
-            brands: () => {
+            brands: async () => {
                 console.log(`SELECT * FROM brands WHERE 
                 ${this.params.id !== undefined ? `id IN (SELECT brand_id FROM category_brand WHERE category_id IN (${(await this.getCategoriesID(this.params.id)).reduce((res, id) => `${res}${res ? ',' : ''}${id}`, '')}))` : '1=1'} AND 
                 ${this.params.search ? `name LIKE '%${this.replaceSpaceToQuery(this.params.search)}' ` : '1=1'} ORDER BY tertip, name`)
